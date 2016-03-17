46fa1fa240fe42a7fff9409c2799dfc5a9452b40
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-17 07:59:56 -0300

    Atualizado o curiosidade10 - Tabuada

--- Curiosidade/curiosidade10-tabuada.py
+++ Curiosidade/curiosidade10-tabuada.py
@@ -3,5 +3,8 @@
 # Trabalhando com tipos diferentes de yiaveis
 
 for y in range(1,11) :
+    print '################'
+    print '# Tabuada',y
+    print '################'
     for x in range(1,11) :
-        print y,' * ',x,' = ',y * x
+        print '# ',y,' * ',x,' = ',y * x
