d3a85a92d162a912725a91740ef5a468ee44d05e
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-22 02:58:13 -0300

    Terminado a explicacao de objeto

--- Novo diretorio/Objetos.py
+++ Novo diretorio/Objetos.py
@@ -6,5 +6,10 @@ print "No Python a linguagem é orientada a objeto. \
 Sendo assim a linguagem possuem atributos e métodos. \
 Tanto atributos quanto os métodos são acessados usando o ponto. \
 \
-Como exemplo: \
-print objeto.atributo"
+Como exemplo. \
+\
+Para mostrar um atributo: print objeto.atributo \
+Para executar método: objeto.metodo(argumentos) \
+Método sem algumento precisa de paramentro: objeto.metodo() \
+\
+Tudo ok ??? :-D"
