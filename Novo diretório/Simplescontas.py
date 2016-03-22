ce2649941c9aba7143a2da598bc8089e3df45fc9
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-22 02:19:38 -0300

    Adicionando novo diretório e os primeiro programas didaticos.

---
+++ Novo diretório/Simplescontas.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# *-* coding: latin1 *-*
+
+
+print 'Operacoes simples.'
+
+print 'Quanto que é 7 x 2 ?', 7 * 2
+
+print 'Trabalhando com variavel.'
+
+print 'variavel pode estár em mais de uma linha: \
+a = 7 * 2 + \\\
+ 4 /2, com o barra invertida'
+
+a = 7 * 2 + \
+4 /2
+
+print "Uma lista quebrada por virgula b = ['a','b','c','d','e'] "
+
+b = ['a','b','c','d','e']
+
+print "Pode conter um range c = range(1,11) "
+
+c = range(1,11)
+
+print 'Podemos mostrar todas as variavel de uma vez. \
+print a,b,c'
+
+print a,b,c
