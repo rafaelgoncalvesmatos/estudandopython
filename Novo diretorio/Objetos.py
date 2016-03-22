21e9a8360d6a37e3b3efb3adda2818935cf5af86
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-22 02:55:41 -0300

    Adicionando explicacao do funcionamento do objeto

--- Novo diretorio/Objetos.py
+++ Novo diretorio/Objetos.py
@@ -2,4 +2,9 @@
 
 # Verificando os objetos
 
-print objeto.atributo
+print "No Python a linguagem é orientada a objeto. \
+Sendo assim a linguagem possuem atributos e métodos. \
+Tanto atributos quanto os métodos são acessados usando o ponto. \
+\
+Como exemplo: \
+print objeto.atributo"
