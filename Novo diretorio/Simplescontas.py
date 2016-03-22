78bbfa8ad93c19abd006b86dcf939feca11c75a6
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-22 02:30:20 -0300

    Adicionado comentario - Simplescontas.py

--- Novo diretorio/Simplescontas.py
+++ Novo diretorio/Simplescontas.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 # *-* coding: latin1 *-*
 
-
+# Programa simples, rastreado pelo github
 print 'Operacoes simples.'
 
+# Trabalhando com opradores
 print 'Quanto que é 7 x 2 ?', 7 * 2
 
+# Passando por variavel, declarando valores a elas
 print 'Trabalhando com variavel.'
 
+# ---------------------------------- INICIO
 print 'variavel pode estár em mais de uma linha: \
 a = 7 * 2 + \\\
  4 /2, com o barra invertida'
@@ -27,3 +30,5 @@ print 'Podemos mostrar todas as variavel de uma vez. \
 print a,b,c'
 
 print a,b,c
+
+# ---------------------------------- INICIO
