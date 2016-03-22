5615f5e10d174f1258fad16e8ef5c6fe3a432eb1
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-22 02:49:02 -0300

    Explorando a estrutura de blocos

--- Novo diretorio/Estrutura_de_bloco.py
+++ Novo diretorio/Estrutura_de_bloco.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # *-* coding: latin1 *-*
+# By: Rafael Gonçalves de Matos
 
 # Estrutura simples de blocos.
 
@@ -17,3 +18,20 @@ for i in [234,654,378,798]:
         print i, ' / 3 =', i/3
 
 print "Não mostrou o 20, porque 20 não é dividido por 3. :-) "
+
+# ---------------------------------------------------------------
+
+print "Vamos prolongar nosso programa. \
+Iremos agora verificar os resultado com numeros 'PAR' dividindo por 2 \
+Terá que mostra apenas numero que são divididos por 2 e a lista será um range"
+
+# Range de 1 a 100
+for i in range(1,101):
+
+    # Se o resto for zero ele é dividido por 2.
+    if i % 2 == 0:
+
+        # Mostre para mim os numero que são divididos por 2.
+        print i, ' / 2 =', i/2
+
+print "Repare que só possui numeros par, FIM ;-)"
