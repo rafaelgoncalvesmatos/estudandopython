ad0293f7632b4374c9fc790bdde06ef52fbddeb9
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-03-29 02:56:11 -0300

    Novos arquivos adicionados.

---
+++ Novo diretorio/Calculando_float.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: latin1 -*-
+
+
+# Calculando numero interiro
+
+print "Utilizando o float para receber as variaveis."
+valor1 = float(raw_input("Primeiro numero: "))
+valor2 = float(raw_input("Segundo numero: "))
+# valor3 = valor1 + valor2
+
+print valor1, " + ", valor2, " = ", valor1+valor2
+
+print "O valor: %d " % (valor1+valor2)
