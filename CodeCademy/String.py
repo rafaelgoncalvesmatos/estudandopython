e4c503579219adab000a11c5cbc098a43524b76e
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-04-29 05:02:35 -0300

    Manipulando strings com o Python

---
+++ CodeCademy/String.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# *-* coding:latin1 *-*
+
+# String
+
+print "Método de string: Permitem a realização de tarefas especificas em strings ( textos )"
+
+# Frase trabalhada
+frase = "Agua de São Frascisco"
+print "A frase que iremos trabalhar é: ", frase
+
+print 'Contando letras com len'
+print len(frase)
+
+print "Caixa baixa com lower"
+print frase.lower()
