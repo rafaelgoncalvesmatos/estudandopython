9aeada0976d62e142d558c7d4672978ad9cc7c1b
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-03 01:09:10 -0300

    Trabalhando com string

--- CodeCademy/String.py
+++ CodeCademy/String.py
@@ -14,3 +14,20 @@ print len(frase)
 
 print "Caixa baixa com lower"
 print frase.lower()
+
+print "Caixa alta com upper"
+print frase.upper()
+
+print "Exemplo de conversao utilizando str()"
+print "O valor de pi e' de cerca de " + str(3.14)
+
+print "Formatacao de string com %"
+name = "Rafael Goncalves"
+print "Olá %s " % (name)
+
+print "Utilizando varias %s (string)"
+name2 = "Amanda Veras"
+print "Eu %s tenho 4 anos de namoro com %s." % (name,name2)
+
+print "Sem utilizacao de variavel"
+print "Eu %s conclui o ensino superior em %s." % ("Rafael","2014")
