52d49d4c03e62b13be3b1633d914ffa6bfeb0e7d
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-04 04:27:43 -0300

    Brincadeira que levou horas e datas - datetime

--- CodeCademy/Date_in_script.py
+++ CodeCademy/Date_in_script.py
@@ -13,6 +13,6 @@ print 'No dia ', now.day
 # varialvel
 
 print 'Exibindo com o operador + e atribuindo a cada variavel o tipo string: ',str(now.day) + '/' + str(now.month) + '/' + str(now.year)
-print 'Exibindo com o operador % fica mais facil de trabalhar - %s, segue: %s-%s-%s' % (now.day,now.month,now.year)
-data = now.day
-print data
+print 'Exibindo com o operador fica mais facil de trabalhar - segue: %s-%s-%s' % (now.day,now.month,now.year), 'Tudo com %s'
+
+print 'Trabalhando com hora: %s:%s:%s' % (now.hour, now.minute, now.second)
