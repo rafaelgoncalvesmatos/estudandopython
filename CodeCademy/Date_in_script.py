b277b18e2b9f0e1789baeaab5455fafc17b6c6a6
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-04 03:52:51 -0300

    Importando lib - datetime

---
+++ CodeCademy/Date_in_script.py
@@ -0,0 +1,11 @@
+#!/usr/bn/python
+# *-* coding:latin1 *-*
+
+from datetime import datetime
+now = datetime.now()
+
+current_year = now.years
+current_month = now.month
+current_day = now.day
+
+print current_year
