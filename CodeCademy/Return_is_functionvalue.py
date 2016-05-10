28359df9a8b56a4a6efe17cc297f23540c048dd6
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-10 05:58:34 -0300

    Script bacana - colocando valores em funcoes, e trabalhando com o return

---
+++ CodeCademy/Return_is_functionvalue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# *-* coding:latin1 *-*
+
+print "Comecando com o funcoes"
+
+def funcao_principal(answer) : 
+	if answer > 5 :
+		return 1
+	elif answer < 5 : 
+		return -1
+	else : 
+		return 0 
+
+print funcao_principal(4)
+print funcao_principal(5)
+print funcao_principal(6)
