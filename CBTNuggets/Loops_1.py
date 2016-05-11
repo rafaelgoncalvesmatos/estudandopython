0498a357b9e46380c0246bd2625adb3e4c83923b
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-11 01:18:13 -0300

    Trabalhando com numero primo

--- CBTNuggets/Loops_1.py
+++ CBTNuggets/Loops_1.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
 # *-* coding:latin1 *-*
 
-num =  raw_input("Digite o numero: ")
+from __future__ import print_function
 
-if num % 2 == 0 : 
-	print "E divisivel por 2!"
-else :
-	print "Não é divisivel por 2!"
+print("Work to Loop. for Rafael G. de Matos")
 
+# raw_input("Digit number: ")
+
+num = 50
+prime = True
+
+for test in range(2,num) :
+	if num % test == 0 and num != test : 
+		print(num, 'equals', test, '*', num/test)
+		prime = False
+		# Colocando o break
+		break
+
+if prime :
+	print(num, 'is a prime number!')
+else : 
+	print(num, 'is not a prime number!')
