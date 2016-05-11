3a9f6d8523393acc0d8f87eb349927e240a6e700
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-11 01:18:39 -0300

    Trabalhando com range

---
+++ CBTNuggets/Range_number.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# *-* coding:latin1 *-*
+
+from __future__ import print_function
+
+# Define range variable
+numbers = range(5,50)
+
+print(numbers)
+
+for number in range(5,50,10) : 
+	print(number)
+
+# OR 
+print("-----------------------------------")
+
+
+numbers1 = range(5,50,10)
+print("numbers1")
+
+for number1 in numbers1 : 
+	print(number1)
