58895fc308782426a4bc3d466debb80b4ce019bd
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-12 06:20:31 -0300

    Update - Final script

--- CodeCademy/Trocando_string.py
+++ CodeCademy/Trocando_string.py
@@ -6,10 +6,12 @@ pyg = 'ay'
 original = raw_input('Entre com uma palavra: ')
 word = original.lower()
 first = word[0]
+new_word = word + first + pyg
+new_word = new_word[1:]
 
 # Checando se o usuário colocou a palavra - isalpha falso caso o usuario digite numero
 if len(original) > 0 and original.isalpha() :
-	print original
+	print new_word
 else :
 	print 'empty'
 
