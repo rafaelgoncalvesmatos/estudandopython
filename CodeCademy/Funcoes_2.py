b49517d0fdd9a6f6f38a4c15fb6892beb455569b
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-17 07:34:54 -0300

    Utilizando estrutura com funcoes

---
+++ CodeCademy/Funcoes_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# *-* coding:latin1 *-*
+
+def shout(phrase) :
+    if phrase == phrase.upper() :
+        return "YOU'RE SHOUTING!"
+    else :
+        return "Can you speak up ?"
+
+shout("I'M INTERESTED IN SHOUTING")
