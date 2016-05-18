aca65edc1a46d999bc57a449ccd6f879ac72f20d
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-18 07:11:02 -0300

    Envia lista e mostra o maior valor

---
+++ CodeCademy/MaiorNumero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# *-* coding:latin1 *-*
+
+num1 = raw_input("1 - ")
+num2 = raw_input("2 - ")
+num3 = raw_input("3 - ")
+
+print "O valor maximo dos numeros é: ",  max(num1, num2, num3)
