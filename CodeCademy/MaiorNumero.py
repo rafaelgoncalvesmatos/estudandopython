81e49107c70ed3ac72e67def45f0460d8e3c28aa
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2016-05-18 23:05:01 -0300

    Adicionando funcao que verifica o tipo de variavel

--- CodeCademy/MaiorNumero.py
+++ CodeCademy/MaiorNumero.py
@@ -6,3 +6,12 @@ num2 = raw_input("2 - ")
 num3 = raw_input("3 - ")
 
 print "O valor maximo dos numeros é: ",  max(num1, num2, num3)
+print "O valor menor dos numeros é: ", min(num1, num2, num3)
+
+# Utilizando o Absolute
+# positivo = raw_input("Digite um numero negativo e será transformado em positivo: ")
+# print abs(positivo)
+
+# Proxima funcao retorna o tipo do caractere digitado
+type1 = raw_input("Que tipo de valor vc digitara agora: ")
+print type(type1)
